05b20d8b71f6e8bed64e1d9e3dc7e6b0523f6f37
Jorin Tielen <jorintielen999@gmail.com>
2018-03-26 11:15:54 +0200

    Started implementing astar

---
+++ gto_myproject/Assets/Scripts/Map/Grid.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+public class Grid
+{
+    private int _width, _height;
+    private Node[,] _grid;
+
+    public Grid(Cell[,] cellMap, int width, int height)
+    {
+        _width = width;
+        _height = height;
+        
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                Cell c = cellMap[x, y];
+                Node n = new Node(new Point(x, y), c.CanMove(), c);
+
+                _grid[x, y] = n;
+            }
+        }
+    }
+
+    public List<Node> GetNeighbours(Node node)
+    {
+        var neighbors = new List<Node>();
+        var pos = node.Position;
+		
+        if (pos.x + 1 < _width)		neighbors.Add(_grid[pos.x + 1, pos.y]);
+        if (pos.x - 1 >= 0) 		neighbors.Add(_grid[pos.x - 1, pos.y]);
+        if (pos.y + 1 < _height) 	neighbors.Add(_grid[pos.x, 	pos.y + 1]);
+        if (pos.y - 1 >= 0)			neighbors.Add(_grid[pos.x, 	pos.y - 1]);
+
+        return neighbors;
+    }
+
+    public Node[,] GetNodeArray()
+    {
+        return _grid;
+    }
+
+    public Node GetNode(Point point)
+    {
+        return GetNode(point.x, point.y);
+    }
+
+    public Node GetNode(int x, int y)
+    {
+        if (x > _width || y > _height)
+        {
+            return null;
+        }
+        
+        return _grid[x, y];
+    }
+}
---
+++ gto_myproject/Assets/Scripts/Map/Node.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+public class Node
+{
+    public Point Position;
+    public bool Walkable;
+    public Cell WorldCell;
+
+    public int GCost;
+    public int HCost;
+    
+    public int FCost
+    {
+        get { return GCost + HCost; }
+    }
+
+    public Node(Point position, bool walkable, Cell worldCell)
+    {
+        Position = position;
+        Walkable = walkable;
+        WorldCell = worldCell;
+    }
+}
